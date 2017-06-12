771b3b2689d8885c1b24b25bf7b32b426236ac13
tR3Nd1 <projecttwo@ya.ru>
2017-06-12 22:20:50 +0700

    Add test for get_sym_freq

--- test/hangman_test.c
+++ test/hangman_test.c
@@ -44,3 +44,55 @@ CTEST(comparsion_string, build_file_name3)
 
 	ASSERT_STR(exp, real);
 }
+
+CTEST(arithmetic_suite, get_sym_freq)
+{
+	char * random = "rain";
+	char * freq = NULL;
+	freq = get_sym_freq(random, freq);
+	int sum = 0;
+	for(int i = 0; i < strlen(random); i++)
+		sum = sum + freq[(unsigned int)random[i]];
+	int i = 4;
+
+	ASSERT_EQUAL(i, sum);
+}
+
+CTEST(arithmetic_suite, get_sym_freq1)
+{
+	char * random = "week";
+	char * freq = NULL;
+	freq = get_sym_freq(random, freq);
+	int sum = 0;
+	for(int i = 0; i < strlen(random); i++)
+		sum = sum + freq[(unsigned int)random[i]];
+	int i = 6;
+
+	ASSERT_EQUAL(i, sum);
+}
+
+CTEST(arithmetic_suite, get_sym_freq2)
+{
+	char * random = "weee";
+	char * freq = NULL;
+	freq = get_sym_freq(random, freq);
+	int sum = 0;
+	for(int i = 0; i < strlen(random); i++)
+		sum = sum + freq[(unsigned int)random[i]];
+	int i = 10;
+
+	ASSERT_EQUAL(i, sum);
+}
+
+CTEST(arithmetic_suite, get_sym_freq3)
+{
+	char * random = "eeee";
+	char * freq = NULL;
+	freq = get_sym_freq(random, freq);
+	int sum = 0;
+	for(int i = 0; i < strlen(random); i++)
+		sum = sum + freq[(unsigned int)random[i]];
+	int i = 16;
+
+	ASSERT_EQUAL(i, sum);
+}
